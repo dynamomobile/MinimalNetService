4a6011e0c3ede03f0088981f14a3ad406db17d75
Edward Patel <edward.patel@memention.com>
2018-02-19 21:21:38 +0100

    First commit

---
+++ NetService/NetService/ViewController.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class ViewController: UIViewController, NetServiceDelegate {
+
+    @IBOutlet var label: UILabel!
+    
+    var ns = NetService.init(domain: "local.", type: "_test._tcp.", name: "My Test")
+
+    func netService(_ sender: NetService, didUpdateTXTRecord data: Data) {
+        let dict = NetService.dictionary(fromTXTRecord: data)
+        if let valueData = dict["value"],
+            let value = String(data: valueData, encoding: .utf8) {
+            label.text = value
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ns.delegate = self
+        ns.startMonitoring()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
---
+++ NetServiceMac/NetServiceMac/ViewController.swift
@@ -0,0 +1,24 @@
+import Cocoa
+
+class ViewController: NSViewController, NetServiceDelegate {
+    
+    @IBOutlet var textField: NSTextField!
+    
+    var ns = NetService(domain: "", type: "_test._tcp.", name: "My Test", port: 0)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ns.delegate = self
+        ns.setTXTRecord(NetService.data(fromTXTRecord: ["value": "0".data(using: .utf8)!]))
+        ns.publish(options: [.listenForConnections, .noAutoRename])
+    }
+
+    @IBAction func buttonPressed(_ sender: Any!) {
+        ns.setTXTRecord(NetService.data(fromTXTRecord: ["value": textField.stringValue.data(using: .utf8)!]))
+    }
+    
+    override var representedObject: Any? {
+        didSet {
+        }
+    }
+}
